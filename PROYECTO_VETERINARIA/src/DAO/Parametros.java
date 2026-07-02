2032624bbb07251038db74bb72ff4d4f839a6b6e
REYJAZ <reynerjesuszegarra28@gmail.com>
2026-07-02 10:43:01 -0500

    commit class

---
+++ PROYECTO_VETERINARIA/src/DAO/Parametros.java
@@ -0,0 +1,10 @@
+
+package DAO;
+public interface Parametros {
+    //TODOS DEBEN TENER EL MISMO NOMBRE DE LA BASE DE DATOS//
+    String URL = "jdbc:mysql://localhost:3306/veterinaria_db";
+    String DRIVER = "com.mysql.cj.jdbc.Driver";
+    String USUARIO = "root";
+    String CLAVE = "";
+    
+}
--- PROYECTO_VETERINARIA/src/PROCESOS/Mensajes.java
+++ PROYECTO_VETERINARIA/src/PROCESOS/Mensajes.java
@@ -1,13 +1,17 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package PROCESOS;
+package Procesos;
+import javax.swing.JOptionPane;
 
-/**
- *
- * @author James
- */
+
 public class Mensajes {
+    public static void M1(String mensaje) {
+        JOptionPane.showMessageDialog(null, mensaje);
+    }
     
+    public static String M2(String mensaje) {
+        return JOptionPane.showInputDialog(mensaje);
+    }
+    
+    public static int M3(String titulo, String mensaje) {
+        return JOptionPane.showConfirmDialog(null, mensaje, titulo, JOptionPane.YES_NO_OPTION);
+    }
 }
