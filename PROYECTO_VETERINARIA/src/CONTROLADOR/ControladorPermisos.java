96f0325251427226ef9b50ebdfdeb1c4b5621c06
ArifJesus <arifsalazar522@gmail.com>
2026-07-02 11:21:09 -0500

    Controladores

--- PROYECTO_VETERINARIA/src/CONTROLADOR/ControladorPermisos.java
+++ PROYECTO_VETERINARIA/src/CONTROLADOR/ControladorPermisos.java
@@ -1,13 +1,50 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package CONTROLADOR;
-
-/**
- *
- * @author PC
- */
+import VISTA.*;
 public class ControladorPermisos {
     
+    public static void AplicarRestricciones(frmMenusVeterinaria vista, int idRol) {
+        vista.MenuClientes.setVisible(false);
+        vista.MenuCitas.setVisible(false);
+        vista.MenuAgendas.setVisible(false);
+        vista.Modificar.setVisible(false);
+        vista.MenuEmpleados.setVisible(false);
+        //ITEMS
+        vista.itemRegistrarCli.setVisible(true);
+        vista.itemCancelar.setVisible(true);
+        vista.itemAgenda.setVisible(true);
+        vista.itemCitas.setVisible(true);
+        vista.itemEstado.setVisible(true);
+        vista.itemAgregar.setVisible(true);
+        vista.itemEliminar.setVisible(true);
+        vista.itemTabla.setVisible(true);
+        //HOLA
+
+        switch (idRol) {
+            case 1: // ADMINISTRADOR (Todo)
+                vista.MenuClientes.setVisible(true);
+                vista.MenuCitas.setVisible(true);
+                vista.MenuAgendas.setVisible(true);
+                vista.Modificar.setVisible(true);
+                vista.MenuEmpleados.setVisible(true);
+                break;
+
+            case 2: // RECEPCIONISTA (Clientes y Citas)
+                vista.MenuClientes.setVisible(true);
+                vista.MenuCitas.setVisible(true);
+                break;
+
+            case 3: // VETERINARIO (Agenda y Procesos)
+                vista.MenuAgendas.setVisible(true);
+                vista.Modificar.setVisible(true);
+                break;
+
+            default:
+                System.out.println("Error: Rol no reconocido.");
+                break;
+        }
+        if (vista.getJMenuBar() != null) {
+            vista.getJMenuBar().revalidate();
+            vista.getJMenuBar().repaint();
+        }
+    }
 }
--- PROYECTO_VETERINARIA/src/FACTORY/VistasFactory.java
+++ PROYECTO_VETERINARIA/src/FACTORY/VistasFactory.java
@@ -1,13 +1,69 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package FACTORY;
-
-/**
- *
- * @author James
- */
+import VISTA.*;
+import CONTROLADOR.*;
+import javax.swing.JDesktopPane;
+import javax.swing.JInternalFrame;
 public class VistasFactory {
+    private static void PresentarVista (JInternalFrame iframe, JDesktopPane contenedor) {
+        contenedor.removeAll();
+        contenedor.add(iframe);
+        contenedor.repaint();
+        int x = (contenedor.getWidth() - iframe.getWidth())/2;
+        int y = (contenedor.getHeight() - iframe.getHeight())/2;
+        iframe.setLocation(x, y);
+        iframe.setVisible(true);
+    }
+    public static void CrearVista (String nomvista, String titulo, JDesktopPane contenedor) {
+        if (nomvista.equals("RegistrarClie")){
+            frmRegistrarClientes FRC = new frmRegistrarClientes();
+            //ControlEstudiante ce = new ControlEstudiante(fes);
+            FRC.setTitle(titulo);
+            PresentarVista(FRC, contenedor);
+        }
+
+
+        if (nomvista.equals("Tabladeempleados")){
+            frmTablaDeEmpleados FTE = new frmTablaDeEmpleados();
+            FTE.setTitle(titulo);
+            PresentarVista(FTE, contenedor);
+        }
+        if (nomvista.equals("EliminarEmpleado")){
+            frmEliminarEmpleado FEE = new frmEliminarEmpleado();
+            FEE.setTitle(titulo);
+            PresentarVista(FEE, contenedor);
+        }
+        if (nomvista.equals("Modificar")){
+            frmModificar FMC = new frmModificar();
+            FMC.setTitle(titulo);
+            PresentarVista(FMC, contenedor);
+        }
+       
+        if (nomvista.equals("")){}
+        
+        if (nomvista.equals("AgregarEmpleado")) { 
+           frmAgregarEmpleado FE = new frmAgregarEmpleado(); 
+           FE.setTitle(titulo);
+           PresentarVista(FE, contenedor); 
+        }
+        if (nomvista.equals("AgendaCitas")) {
+           frmAgendaDeCita FA = new frmAgendaDeCita(); // Asegúrate de que este nombre de clase coincida con tu carpeta VISTA
+           FA.setTitle(titulo);
+           PresentarVista(FA, contenedor);
+        }
+    
+        if (nomvista.equals("CancelarCita")){
+           frmCancelarCita FCC = new frmCancelarCita();
+           FCC.setTitle(titulo);
+           PresentarVista(FCC, contenedor);
+        }
+
+
+        if (nomvista.equals("AgendasAsignadas")){
+            frmAgendadeCitasAsignadas FACA = new frmAgendadeCitasAsignadas();
+            FACA.setTitle(titulo);
+            PresentarVista(FACA, contenedor);
+        }
+
+    }   
     
 }
